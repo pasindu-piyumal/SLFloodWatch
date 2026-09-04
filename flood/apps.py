e86d69e9944e3c98556bb2ee85f4bcb4627dcf03
pasindu-piyumal <22cds0423@ms.sab.ac.lk>
2026-09-04 21:18:19 +0530

    add app configuration for flood monitoring

---
+++ flood/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class FloodConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "flood"
+    verbose_name = "Flood Monitoring"
+
