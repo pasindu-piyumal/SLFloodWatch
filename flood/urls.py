eb16e00655fee33e5a6cad7714b0643c362822c6
pasindu-piyumal <22cds0423@ms.sab.ac.lk>
2026-09-13 11:24:40 +0530

    configure URL patterns for locations, and location details

--- flood/urls.py
+++ flood/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard')
+    path('', views.dashboard, name='dashboard'),
+    path('locations/', views.locations, name='locations'),
+    path('locations/<int:pk>/', views.location_detail, name='location_detail')
 ]
